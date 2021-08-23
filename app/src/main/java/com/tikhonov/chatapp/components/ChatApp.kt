6ae17b6cb31a7982b37e345d51116020cf352304
sergiy-tikhonov <sergey.tikhonov.wwind@gmail.com>
2021-08-23 12:46:21 +0300

    Made UserArea component

--- app/src/main/java/com/tikhonov/chatapp/components/ChatApp.kt
+++ app/src/main/java/com/tikhonov/chatapp/components/ChatApp.kt
@@ -1,16 +1,67 @@
 package com.tikhonov.chatapp.components
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import android.util.Log
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
+import com.tikhonov.chatapp.R
+import androidx.compose.runtime.getValue // <-- WITHOUT THAT IT WOULDN'T WORK
+import androidx.compose.runtime.setValue // <-- WITHOUT THAT IT WOULDN'T WORK
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tikhonov.chatapp.ui.theme.ChatAppTheme
 
 @Composable
 fun ChatApp() {
+    var user1Message by remember { mutableStateOf("") }
+    var user2Message by remember { mutableStateOf("") }
+    var chat: MutableList<String> by remember {
+        mutableStateOf(mutableListOf())
+    }
     Column() {
-        Row() {
-            UserArea(userName = "User1")
-            UserArea(userName = "User2")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            UserArea(
+                userName = stringResource(id = R.string.user1_name),
+                avatar = R.drawable.boy,
+                message = user1Message,
+                onMessageChanged = {
+                    user1Message = it
+                },
+                modifier = Modifier.weight(1.0f),
+                onMessageAdd = {
+                    chat.add(user1Message)
+                    user1Message = ""
+                }
+            )
+            Spacer(
+                modifier = Modifier.width(16.dp)
+            )
+            UserArea(
+                userName = stringResource(id = R.string.user2_name),
+                avatar = R.drawable.girl,
+                message = user2Message,
+                onMessageChanged = {
+                    user2Message = it
+                },
+                modifier = Modifier.weight(1.0f),
+                onMessageAdd = {
+                    chat.add(user2Message)
+                    user2Message = ""
+                }
+            )
         }
         ChatArea()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultChatAppPreview() {
+    ChatAppTheme {
+        ChatApp()
     }
 }
--- app/src/main/java/com/tikhonov/chatapp/components/UserArea.kt
+++ app/src/main/java/com/tikhonov/chatapp/components/UserArea.kt
@@ -1,9 +1,97 @@
 package com.tikhonov.chatapp.components
 
-import androidx.compose.material.Text
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tikhonov.chatapp.R
+import com.tikhonov.chatapp.ui.theme.Shapes
 
 @Composable
-fun UserArea(userName: String) {
-    Text(text = userName)
+fun UserArea(
+    userName: String,
+    @DrawableRes avatar: Int,
+    message: String,
+    onMessageChanged: (String) -> Unit,
+    modifier: Modifier = Modifier.width(150.dp),
+    onMessageAdd: () -> Unit
+) {
+    Card(
+        border = BorderStroke(width = 1.dp, color = Color.DarkGray),
+        shape = Shapes.medium,
+        backgroundColor = Color.LightGray,
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ){
+                Text(
+                    text = userName,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Image(
+                    painter = painterResource(id = avatar),
+                    contentDescription = "Person image",
+                    modifier = Modifier.size(100.dp),
+                    alignment = Alignment.TopCenter
+                )
+            }
+
+            Row() {
+                OutlinedTextField(
+                    value = message,
+                    onValueChange = onMessageChanged,
+                    modifier = Modifier.weight(1.0f),
+                    label = {
+                        Text(text = "Message") },
+                    trailingIcon = {
+                        Icon(
+                            Icons.Outlined.Send,
+                            contentDescription = "Send Message",
+                            modifier = Modifier
+                                .clickable(
+                                    onClick = onMessageAdd
+                                )
+                                .background(Color.Gray),
+                            tint = Color.White
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserAreaPreview() {
+    UserArea(
+        userName = "User1",
+        avatar = R.drawable.boy,
+        message = "Hello!",
+        onMessageChanged = {},
+        onMessageAdd = {}
+        )
 }
